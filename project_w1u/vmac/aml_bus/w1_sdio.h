f7b6571d8eed68274492635ca970dd3db918354a
gongwei.chen <gongwei.chen@amlogic.com>
2024-12-06 14:34:45 +0800

    Revert "wifi: fixed openlinux bt suspend failed [1/1]"
    This reverts commit 01c36dffa6f97e481e414bc8e0853e2c94a5fe75.
    
    PD#SWPL-195446
    
    Change-Id: Ic27f9e5a99d72c6f74b36dbff369f406b38dfd2e
    Signed-off-by: gongwei.chen <gongwei.chen@amlogic.com>

--- project_w1u/vmac/aml_bus/w1_sdio.h
+++ project_w1u/vmac/aml_bus/w1_sdio.h
@@ -221,8 +221,8 @@ struct amlw1_hif_ops {
 
 struct aml_hif_sdio_ops {
     //sdio func0 for self define domain, cmd52, func0 is added to adapt to w2, not needed on w1u
-    //int (*hi_self_define_domain_func0_write8)(int addr, unsigned char data);
-    //unsigned char (*hi_self_define_domain_func0_read8)(int addr);
+    int (*hi_self_define_domain_func0_write8)(int addr, unsigned char data);
+    unsigned char (*hi_self_define_domain_func0_read8)(int addr);
     //sdio func1 for self define domain, cmd52
     int (*hi_self_define_domain_write8)(int addr, unsigned char data);
     unsigned char (*hi_self_define_domain_read8)(int addr);
